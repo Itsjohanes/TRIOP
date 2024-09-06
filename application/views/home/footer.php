cdaa5d73779d3e5b20ddbb305f91798e294ec385
Johannes Alexander Putra <71767003+Itsjohanes@users.noreply.github.com>
2024-09-06 08:02:14 +0700

    perbaikan footer

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -1,11 +1,15 @@
-    <footer class="page-footer dark">
-      <div class="footer-copyright">
-        <h5>© 2025 TIM PUBDOK TRINITAS OPEN</h5>
-      </div>
-    </footer>
-    <script src="<?php base_url().'assets/bootstrap/js/bootstrap.min.js'?>"></script>
-    <script src="<?php base_url().'assets/js/baguetteBox.min.js'?>"></script>
-    <script src="<?php base_url().'assets/js/vanilla-zoom.js'?>"></script>
-    <script src="<?php base_url().'assets/js/theme.js'?>"></script>
-  </body>
+<footer class="page-footer dark">
+  <div class="footer-content">
+    <h5>© 2025 TIM PUBDOK TRINITAS OPEN</h5>
+    <h5>SMA Trinitas Bandung, Jalan Kebon Jati No. 209, Bandung</h5>
+      <a href="https://ppdb.smatrinitas.sch.id" target="_blank">Pendaftaran PPDB</a> | 
+      <a href="https://smatrinitas.sch.id" target="_blank">Website SMA Trinitas</a>
+  </div>
+</footer>
+
+<script src="<?php echo base_url().'assets/bootstrap/js/bootstrap.min.js'; ?>"></script>
+<script src="<?php echo base_url().'assets/js/baguetteBox.min.js'; ?>"></script>
+<script src="<?php echo base_url().'assets/js/vanilla-zoom.js'; ?>"></script>
+<script src="<?php echo base_url().'assets/js/theme.js'; ?>"></script>
+</body>
 </html>
