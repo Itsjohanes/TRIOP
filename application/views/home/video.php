439842c8a8331769aafab11a5c779fd499a1fb49
johannes <johannesap@upi.edu>
2024-09-22 06:19:23 +0700

    perbaikan video

--- application/views/home/video.php
+++ application/views/home/video.php
@@ -1,37 +1,29 @@
-
-    <main class="page">
-      <section class="clean-block clean-pricing dark">
-        <div class="container">
-          <div class="block-heading">
-            <h2 class="text-info">Video Pertandingan</h2>
-           
-          </div>
-          <div class="row justify-content-center">
-            <!-- Video 1 -->
-                <!-- Video 1 -->
-               <!-- Begin Page Content -->
-            <div class="container-fluid">
-                <div class="row">
-                    <?php foreach ($video as $v): ?>
-                        <div class="col-md-5 col-lg-4">
-                            <div class="clean-pricing-item">
-                                <h4><?= htmlspecialchars($v['judul']); ?></h4>
-                                <div class="embed-responsive embed-responsive-16by9">
-                                 
-                                    <iframe
-                                        class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/<?= $v['link']; ?>"
-                                        allowfullscreen
-                                    ></iframe>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endforeach; ?>
+<main class="page">
+  <section class="clean-block clean-pricing dark">
+    <div class="container">
+      <div class="block-heading">
+        <h2 class="text-info">Video Pertandingan</h2>
+      </div>
+      <div class="row justify-content-center">
+        <!-- Video 1 -->
+        <div class="container-fluid">
+          <div class="row">
+            <?php foreach ($video as $v): ?>
+              <div class="col-md-5 col-lg-4 mb-4">
+                <div class="clean-pricing-item">
+                  <h4><?= htmlspecialchars($v['judul']); ?></h4>
+                  <div class="ratio ratio-16x9">
+                    <iframe
+                      src="https://www.youtube.com/embed/<?= $v['link']; ?>"
+                      allowfullscreen
+                    ></iframe>
+                  </div>
                 </div>
-            </div>
-<!-- End of Page Content -->
-
+              </div>
+            <?php endforeach; ?>
           </div>
         </div>
-      </section>
-    </main>
+      </div>
+    </div>
+  </section>
+</main>
