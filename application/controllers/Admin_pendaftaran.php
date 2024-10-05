2741a69064bd60b19dc63157525a6a3f5660afbf
Johannes Alexander Putra, B.Ed. <71767003+Itsjohanes@users.noreply.github.com>
2024-10-05 09:37:25 +0700

    Update Admin_pendaftaran.php

--- application/controllers/Admin_pendaftaran.php
+++ application/controllers/Admin_pendaftaran.php
@@ -73,6 +73,18 @@ class Admin_pendaftaran extends CI_Controller
         $this->load->view('admin_pendaftaran/pendaftaran');
         $this->load->view('admin_pendaftaran/footer');
     }
+	public function ubahStatusPendaftaran()
+    {
+        $status = $this->input->post('status');
+        $newStatus = $status == 1 ? 0 : 1;  // Toggle status
+    
+        $this->db->set('status', $newStatus);
+        $this->db->where('id_aktifpendaftaran', 1); // Assuming there’s only one entry for registration status
+        $this->db->update('tb_aktifpendaftaran'); 
+    
+        $this->session->set_flashdata('category_success', 'Status pendaftaran berhasil diubah.');
+        redirect('admin_pendaftaran/pendaftaran');
+    }
     public function hapus_pendaftaran($id) {
     // Ambil data pendaftaran berdasarkan ID
     $pendaftaran = $this->Pendaftaran_model->get_pendaftaran_by_id($id);
